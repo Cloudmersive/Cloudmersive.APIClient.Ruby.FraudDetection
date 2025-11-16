e08d3193753efff6dd1bd666744e335e80156505
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2025-11-16 01:31:23 +0000

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -3,15 +3,15 @@ Remove-Item *.gem
 Remove-Item ./client/*.gem
 
 
-Invoke-WebRequest -Uri 'https://api.cloudmersive.com/cdr/docs/v1/swagger' -OutFile '.\cdr-api-swagger.json'
-(Get-Content .\cdr-api-swagger.json).replace('localhost', "api.cloudmersive.com") | Set-Content .\cdr-api-swagger.json
-(Get-Content .\cdr-api-swagger.json -Raw) -replace '"http"','"https"' | Set-Content .\cdr-api-swagger.json -Encoding UTF8
+Invoke-WebRequest -Uri 'https://api.cloudmersive.com/fraud-ai/docs/v1/swagger' -OutFile '.\fraud-api-swagger.json'
+(Get-Content .\fraud-api-swagger.json).replace('localhost', "api.cloudmersive.com") | Set-Content .\fraud-api-swagger.json
+(Get-Content .\fraud-api-swagger.json -Raw) -replace '"http"','"https"' | Set-Content .\fraud-api-swagger.json -Encoding UTF8
 
-& java -jar swagger-codegen-cli-2.4.14.jar generate -i .\cdr-api-swagger.json -l ruby -o client -c packageconfig.json
+& java -jar swagger-codegen-cli-2.4.14.jar generate -i .\fraud-api-swagger.json -l ruby -o client -c packageconfig.json
 #(Get-Content ./client/src/api/ConvertDocumentApi.js).replace('var returnType = Object;', "var returnType = 'Blob';") | Set-Content ./client/src/api/ConvertDocumentApi.js
 #(Get-Content ./client/src/api/ConvertWebApi.js).replace('var returnType = Object;', "var returnType = 'Blob';") | Set-Content ./client/src/api/ConvertWebApi.js
 
-(Get-Content ./client/cloudmersive-cdr-api-client.gemspec).replace('`find *`.split("\n").uniq.sort.select { |f| !f.empty? }', "Dir['./**/*']") | Set-Content ./client/cloudmersive-cdr-api-client.gemspec
+(Get-Content ./client/cloudmersive-frauddetection-api-client.gemspec).replace('`find *`.split("\n").uniq.sort.select { |f| !f.empty? }', "Dir['./**/*']") | Set-Content ./client/cloudmersive-frauddetection-api-client.gemspec
 #& npm build ./client
 
 
@@ -20,6 +20,6 @@ Invoke-WebRequest -Uri 'https://api.cloudmersive.com/cdr/docs/v1/swagger' -OutFi
 Push-Location
 & cd ./client
 
-& gem build ./cloudmersive-cdr-api-client.gemspec
+& gem build ./cloudmersive-frauddetection-api-client.gemspec
 
 Pop-Location
